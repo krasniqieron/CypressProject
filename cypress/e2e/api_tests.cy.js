95925792033f678f7f5808e651379d31c6861518
EronKrasniqi <krasniqi.eron7@gmail.com>
2024-09-01 12:11:07 +0200

    Added additional test cases for API Testing

--- cypress/e2e/api_tests.cy.js
+++ cypress/e2e/api_tests.cy.js
@@ -39,5 +39,60 @@ describe('API Testing for Authentication and Deposit', () => {
       });
     });
   
+    it('should perform a witdrawal request', () => {
+        expect(authToken).to.not.be.undefined;
+    
+  
+        cy.request({
+          method: 'POST',
+          url: 'https://api.amega.finance/withdrawal', 
+          headers: {
+            'Authorization': `Bearer ${authToken}`
+          },
+          body: {
+            amount: 1000,
+            currency: 'USD' 
+          }
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property('success', true);
+        });
+      });
+
+      it('should perform a transfer request', () => {
+        expect(authToken).to.not.be.undefined;
+    
+  
+        cy.request({
+          method: 'POST',
+          url: 'https://api.amega.finance/transfer', 
+          headers: {
+            'Authorization': `Bearer ${authToken}`
+          },
+          body: {
+            amount: 1000,
+            currency: 'USD' 
+          }
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property('success', true);
+        });
+      });
+
+      it('should retrieve the transaction history', () => {
+    
+        cy.request({
+          method: 'GET',
+          url: 'https://www.amega.finance/api/transactions/transaction-history', 
+          headers: {
+            Authorization: `Bearer ${authToken}` 
+          }
+        }).then((response) => {
+          expect(response.status).to.eq(200); 
+          expect(response.body).to.have.property('transactions'); 
+          
+          cy.log(JSON.stringify(response.body)); 
+        });
+      });
   });
   
